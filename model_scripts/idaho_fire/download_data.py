7e8c98b26b587d3ea21e841d75ee5c0afdd0a170
Shawn Taylor <shawntaylor@weecology.org>
2018-01-27 13:30:51 -0500

    download and convert modis data

---
+++ model_scripts/idaho_fire/download_data.py
@@ -0,0 +1,50 @@
+from getpass import getpass
+import os
+import glob
+from subprocess import call
+
+# This downloads the raw hdf modis file used in the idaho fire succession model. You will need a NASA Earthdata
+# login to download from the FTP (available for free)
+
+download_dir = '../../raw_data/idaho_fire/downloaded_modis/'
+
+directory_exists = os.path.isdir(download_dir)
+if not directory_exists:
+    os.makedirs(download_dir)
+
+file_list=['https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2001.01.01/MCD12Q1.A2001001.h09v04.051.2014287162024.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2002.01.01/MCD12Q1.A2002001.h09v04.051.2014287170022.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2003.01.01/MCD12Q1.A2003001.h09v04.051.2014287182211.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2004.01.01/MCD12Q1.A2004001.h09v04.051.2014287174031.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2005.01.01/MCD12Q1.A2005001.h09v04.051.2014287190241.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2006.01.01/MCD12Q1.A2006001.h09v04.051.2014287194338.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2007.01.01/MCD12Q1.A2007001.h09v04.051.2014287211100.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2008.01.01/MCD12Q1.A2008001.h09v04.051.2014288150448.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2009.01.01/MCD12Q1.A2009001.h09v04.051.2014288175117.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2010.01.01/MCD12Q1.A2010001.h09v04.051.2014288182320.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2011.01.01/MCD12Q1.A2011001.h09v04.051.2014288190332.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2012.01.01/MCD12Q1.A2012001.h09v04.051.2014288200052.hdf',
+           'https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.051/2013.01.01/MCD12Q1.A2013001.h09v04.051.2014308185258.hdf']
+
+
+username = input('NASA EarthLogin username: ')
+password  = getpass('NASA Earthlogin password: ')
+
+total_files = len(file_list)
+for i, this_file in enumerate(file_list,1):
+    print('downloading '+str(i)+' of '+str(total_files))
+    os.system('wget --user={0} --password={1} -P {2} {3}'.format(username, password, download_dir, this_file))
+####################################################################################
+# Next convert them to more friendly tif format using the MODIS Reprojection tool
+# https://lpdaac.usgs.gov/tools/modis_reprojection_tool
+mrtPath='/home/shawn/bin/bin/resample'
+resample_config_file='./resample_config.prm'
+
+hdfFiles = glob.glob(download_dir+'/*hdf')
+
+for this_file in hdfFiles:
+    #Keeps the 1st 3 sections of the modis filename. Like MOD13A1-A2008144-h10v04
+    output_file=download_dir+'-'.join(os.path.basename(this_file).split('.')[0:3])+'.tif'
+    call([mrtPath, '-i', this_file, '-o', output_file, '-p', resample_config_file ])
+
+
