5321995202a614255bf814e8fd19f0fe1ccf86bc
Shawn Taylor <shawntaylor@weecology.org>
2018-01-27 13:35:11 -0500

    crop to final data subset

---
+++ model_scripts/idaho_fire/crop_raster_data.R
@@ -0,0 +1,35 @@
+#Take the original modis landcover rasters for each year and crop them to
+#the training and testng areas
+library(raster)
+library(rgdal)
+library(sp)
+
+raw_modis_dir='./raw_data/idaho_fire/downloaded_modis/'
+
+file_list=list.files(raw_modis_dir, pattern='*tif', full.names = TRUE)
+
+file_list=file_list[!grepl('xml', file_list)]
+file_list=file_list[!grepl('ovr', file_list)]
+
+
+study_area=readOGR('./raw_data/idaho_fire/gis/', 'study_area')
+
+#Crop training years
+for(this_year in 2001:2004){
+  file_path=grep(paste0('A',this_year), file_list, value=TRUE)
+  whole_raster=raster::raster(file_path)
+  cropped_raster=crop(whole_raster, study_area)
+
+  filename=paste0(this_year,'.tif')
+  writeRaster(cropped_raster, paste0('./raw_data/idaho_fire/training/',filename))
+}
+
+#Crop testing years
+for(this_year in 2005:2013){
+  file_path=grep(paste0('A',this_year), file_list, value=TRUE)
+  whole_raster=raster::raster(file_path)
+  cropped_raster=crop(whole_raster, study_area)
+  
+  filename=paste0(this_year,'.tif')
+  writeRaster(cropped_raster, paste0('./data/idaho_fire/testing/',filename))
+}
