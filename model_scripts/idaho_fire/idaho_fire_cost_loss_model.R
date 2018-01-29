da5e8f8eda27a7084dc1e6447418417cfd7a2dc3
Shawn Taylor <shawntaylor@weecology.org>
2018-01-29 11:28:44 -0500

    add time component to idaho fire

--- model_scripts/idaho_fire/idaho_fire_cost_loss_model.R
+++ model_scripts/idaho_fire/idaho_fire_cost_loss_model.R
@@ -3,8 +3,9 @@ library(tidyverse)
 
 #The per year per cell costs
 treatment_cost = 10
-possible_loss_costs = 10 / seq(0.01, 0.7, 0.01)
+possible_loss_costs = 10 / seq(0.01, 0.99, 0.02)
 
+testing_years = 2008:2013
 
 traditional_metrics_file = 'results/idaho_fire_metrics.csv'
 cost_loss_value_file = 'results/idaho_fire_cost_loss_results.csv'
@@ -18,6 +19,12 @@ observation_rasters = observation_rasters[!grepl('xml', observation_rasters)]
 
 forecasts = raster::stack(forecast_rasters)
 observations = raster::stack(observation_rasters)
+
+# changes layer names from 2008_prediction -> 2008 and get a list of them
+for(i in 1:length(forecasts@layers)){
+  forecasts@layers[[i]]@data@names =  stringr::word(forecasts@layers[[i]]@data@names,1,1,sep='_')
+  observations@layers[[i]]@data@names =  stringr::word(observations@layers[[i]]@data@names,1,1,sep='_')
+}
 
 #Total 500x500m cells. Rows * columns * years
 total_cells = prod(dim(forecasts))
@@ -74,28 +81,32 @@ false_negative_costs = function(forecast, observation, L){
 #Calculate cost/loss model curve
 cost_loss_values=data.frame()
 for(loss_cost in possible_loss_costs){
-  # Get a binary prediction map based on this cost/loss ratio
-  a = treatment_cost/loss_cost
-  forecasts_binary = raster::calc(forecasts, function(x, threshold){(x>=a)*1})
-  
-  expense_perfect = (sum(as.vector(observations)) * treatment_cost) / total_cells
-  expense_never = (sum(as.vector(observations)) * loss_cost) / total_cells
-  expense_maximimum = min(treatment_cost, expense_never)
-  
-  forecast_treatment_cost = (sum(as.vector(forecasts_binary)) * treatment_cost)
-  forecast_fn_cost = false_negative_costs(forecasts_binary, observations, loss_cost)
-  forecast_expense = (forecast_treatment_cost + forecast_fn_cost) / total_cells
-  
-  cost_loss_values = cost_loss_values %>%
-    bind_rows(data.frame('a' = treatment_cost / loss_cost,
-                         'expense_max' = expense_maximimum,
-                         'expense_perfect' = expense_perfect, 
-                         'expense_forecast' = forecast_expense))
+  for(year in testing_years){
+    year_layer = paste0('X',year)
+    # Get a binary prediction map based on this cost/loss ratio
+    a = treatment_cost/loss_cost
+    forecasts_binary = raster::calc(forecasts[[year_layer]], function(x, threshold){(x>=a)*1})
+    
+    expense_perfect = (sum(as.vector(observations[[year_layer]])) * treatment_cost) / total_cells
+    expense_never = (sum(as.vector(observations[[year_layer]])) * loss_cost) / total_cells
+    expense_maximimum = min(treatment_cost, expense_never)
+    
+    forecast_treatment_cost = (sum(as.vector(forecasts_binary)) * treatment_cost)
+    forecast_fn_cost = false_negative_costs(forecasts_binary, observations[[year_layer]], loss_cost)
+    forecast_expense = (forecast_treatment_cost + forecast_fn_cost) / total_cells
+    
+    cost_loss_values = cost_loss_values %>%
+      bind_rows(data.frame('a' = treatment_cost / loss_cost,
+                           'year' = year,
+                           'expense_max' = expense_maximimum,
+                           'expense_perfect' = expense_perfect, 
+                           'expense_forecast' = forecast_expense))
+  }
 }
 
 cost_loss_values$value = with(cost_loss_values, (expense_max - expense_forecast) / (expense_max - expense_perfect))
 
-ggplot(cost_loss_values, aes(x=a, y=value)) + 
+ggplot(cost_loss_values, aes(x=year, y=value, color=a, group=a)) + 
   ylim(0,1) +
   geom_line(size=1.5)+
   scale_linetype_manual(values = c('solid','dashed','dotted')) +
@@ -109,6 +120,24 @@ ggplot(cost_loss_values, aes(x=a, y=value)) +
         legend.position = "bottom", 
         legend.direction = "horizontal",
         legend.key.width = unit(35, units = 'mm')) +
+  labs(title = "Idaho Fire Cost Loss Analysis") 
+
+ggplot(cost_loss_values, aes(x=a, y=value)) + 
+  ylim(-10,1) +
+  xlim(0,1) +
+  geom_line(size=1.5)+
+  scale_linetype_manual(values = c('solid','dashed','dotted')) +
+  theme(plot.title = element_text(size = 30),
+        axis.text = element_text(size = 20),
+        axis.title = element_text(size = 22),
+        legend.text = element_text(size = 15), 
+        legend.title = element_text(size = 20),
+        strip.text.x=element_text(size=22),
+        strip.text.y=element_text(size=22),
+        legend.position = "bottom", 
+        legend.direction = "horizontal",
+        legend.key.width = unit(35, units = 'mm')) +
+  facet_grid(~year)+
   labs(title = "Idaho Fire Cost Loss Analysis") 
 
 write_csv(cost_loss_values, cost_loss_value_file)
