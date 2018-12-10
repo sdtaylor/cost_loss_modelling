1ef7c2deadd057c0a17f24d993e51d0426227bdf
Shawn Taylor <shawntaylor@weecology.org>
2018-12-10 11:48:35 -0500

    add bbs select species values, drop ID-fire stuff

--- result_figures.R
+++ result_figures.R
@@ -29,74 +29,45 @@ ggsave(portal_fig, filename = 'manuscript/portal_cost_loss_values.png', height=5
 
 ##################################################
 ##################################################
-# Idaho fire
-idaho_cost_loss_values = read_csv('results/idaho_fire_cost_loss_results.csv')
-
-idaho_value = ggplot(idaho_cost_loss_values, aes(x=a, y=value, color=as.factor(year), group=year)) + 
-  ylim(0.001,1) +
-  xlim(0,1) +
-  geom_line(size=2)+
-  #scale_color_brewer(palette = 'Greys') +
-  scale_color_viridis(discrete = TRUE, direction = -1, option = 'inferno', end=0.94) +
-  theme_bw() +
-  theme(plot.title = element_text(size = 30),
-        axis.text = element_text(size = 20),
-        axis.title = element_text(size = 22),
-        legend.text = element_text(size = 15), 
-        legend.title = element_text(size = 20),
-        strip.text.x=element_text(size=22),
-        strip.text.y=element_text(size=22),
-        legend.position = c(0.8, 0.8), 
-        legend.direction = "vertical",
-        legend.key.width = unit(15, units = 'mm'),
-        legend.key.height = unit(9, units='mm')) +
-  labs(title = "Idaho Fire Cost Loss Analysis",
-       x='a = C/L',y='Value', color='Year') 
-
-ggsave(idaho_value, filename = 'manuscript/idaho_cost_loss_value.png',height=8, width=10)
-
-# Value over time
-a_ratios_of_interest = c(0.01, 0.25, 0.51, 0.63)
-idaho_value_over_time = ggplot(filter(idaho_cost_loss_values, a %in% a_ratios_of_interest), aes(x=year, y=value, color=as.factor(a), group=a)) + 
-  ylim(-0.5,1) +
-  geom_line(size=2.5)+
-  geom_hline(yintercept = 0, size=2) +
-  scale_color_brewer(palette='Dark2') +
-  theme_bw() +
-  theme(plot.title = element_text(size = 30),
-        axis.text = element_text(size = 20),
-        axis.title = element_text(size = 22),
-        legend.text = element_text(size = 20), 
-        legend.title = element_text(size = 23),
-        strip.text.x=element_text(size=22),
-        strip.text.y=element_text(size=22),
-        legend.position =  c(0.5, 0.17), 
-        legend.direction = "vertical",
-        legend.background = element_rect(fill='grey95'),
-        legend.key.width = unit(35, units = 'mm'),
-        legend.key.height = unit(10, units='mm')) +
-  labs(title = "Idaho Fire cost loss values over time",
-       x='Year', y = 'Value', color='a = C/L') 
-
-ggsave(idaho_value_over_time, filename = 'manuscript/idaho_value_over_time.png',height=8, width=10)
-
-both_idaho_plots = cowplot::plot_grid(idaho_value, idaho_value_over_time,
-                                      labels=c('A.','B.'), ncol=1, label_size = 30)
-ggsave(both_idaho_plots, filename='manuscript/idaho_value.png',height=16, width=10)
-
-##################################################
-##################################################
 # BBS
 
-bbs_cost_loss_results = read_csv('results/bbs_cost_loss_results.csv')
+bbs_cost_loss_results = read_csv('results/bbs_cost_loss_results.csv') %>%
+  filter(threshold_type %in% c('max_specificity_sensitivity_base_probability', 'cl_ratio_base_probability'))
 bbs_species_names = read_csv('raw_data/bbs/BBS50_species.csv') %>%
   select(Aou = AOU, species = english_common_name)
 
 bbs_cost_loss_results = bbs_cost_loss_results %>%
   left_join(bbs_species_names, by='Aou')
 
+selected_species = c(
+  6450, #  Nashville Warbler
+  6882, # House Sparrow
+  5930, # Northern Cardinal
+  5010  # Eastern Meadowlark
+)
+
+bbs_select_spp_value = ggplot(filter(bbs_cost_loss_results, Aou %in% selected_species), aes(x=a, y=value, color=threshold_type)) +
+  geom_line(size=2) + 
+  scale_color_manual(values=c('#0072B2','#D55E00')) + 
+  ylim(0.01,1) + 
+  facet_wrap(~species) + 
+  labs(y='Value', x = 'Cost/Loss Ratio (a)', color='Threshold Type') + 
+  theme_bw() + 
+  theme(strip.text = element_text(size=15),
+        axis.text =  element_text(size=13),
+        axis.title = element_text(size=15),
+        legend.position = c(0.75, y=0.88),
+        legend.background = element_rect(color='black',fill='grey99'),
+        legend.key.width = unit(15,'mm'),
+        legend.title = element_text(size=13),
+        legend.text = element_text(size=10))
+
+ggsave(bbs_select_spp_value, filename='manuscript/bbs_select_spp_value.png',height=20, width=23, units='cm', dpi=100)
+
 ##############
-# Plot of avg value
+# Plot of quantile values
+# Three quantiles, and the maximum, value for all xxx BBS species over all possible C/L values.
+# The minimum value for any given C/L is below 0, signifying a forecast provides no value over taking naive actions.
 bbs_median_values = bbs_cost_loss_results %>%
   group_by(a) %>%
   summarize('50%' = quantile(value, 0.50),
@@ -123,8 +94,9 @@ ggsave(bbs_quantile_values, filename = 'manuscript/bbs_quantile_values.png', hei
 
 ###############
 # Supplement plot of value for every species
-bbs_all_spp_values = ggplot(bbs_cost_loss_results, aes(x=a, y=value)) +
+bbs_all_spp_values = ggplot(bbs_cost_loss_results, aes(x=a, y=value, color=threshold_type)) +
   geom_line() + 
+  #scale_color_manual(values=c('black','#56B4E9')) + 
   ylim(0,1) + 
   facet_wrap(~species) + 
   labs(y='Value', x = 'a = cost/loss') + 
